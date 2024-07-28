7264e829e45a36c3e9f33c3f62b9b57e739138f9
preck273 <72576947+preck273@users.noreply.github.com>
2024-07-28 22:18:58 +0200

    font

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4 px-6 flex flex-col md:flex-row items-center justify-between">
       <div className="mb-4 md:mb-0"> 
-        <p className="text-xs md:text-sm">&copy; 2024 Oziwo</p> 
+        <p className="text-sm md:text-sm">&copy; 2024 Oziwo</p> 
       </div>
       <div className="flex items-center space-x-10 mb-4 md:mb-0"> 
         <a href="https://linkedin.com/in/precious-oziwo-708363228/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white">
@@ -17,11 +17,11 @@ export default function Footer() {
       <div className="flex items-center space-x-4">
         <div className="flex items-center">
           <i className="fas fa-envelope fa-lg"></i>
-          <p className="text-xs md:text-sm ml-1">Pepelito273@outlook.com</p>
+          <p className="text-sm md:text-sm ml-1">Pepelito273@outlook.com</p>
         </div>
         <div className="flex items-center">
           <i className="fas fa-phone fa-lg"></i>
-          <p className="text-xs md:text-sm ml-1">0685135674</p> 
+          <p className="text-sm md:text-sm ml-1">0685135674</p> 
         </div>
       </div>
     </footer>
